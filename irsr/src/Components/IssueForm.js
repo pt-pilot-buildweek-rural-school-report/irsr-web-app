8b1111a33c0016bf805d3aa8b031fcdffe77c864
Andrew <andrewryanogle@gmail.com>
2019-02-13 22:37:28 -0800

    started working on the issue submission form

---
+++ irsr/src/Components/IssueForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class IssueForm extends Component {
+    state = {
+        newIssue: {
+            name: '',
+            date: '',
+            issue: '',
+            discription: ''
+        }
+    }
+
+    handleChange = event => {
+        this.setState({
+            newIssue: {
+                ...this.state.newIssue,
+                [event.target.name]: event.target.value
+            }
+        });
+    }
+    render() {
+        return (<div className='issueForm'>
+
+            <form>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onChange={this.handleChange}
+                    value={this.state.newIssue.name}
+                />
+                <input
+                    type="text"
+                    name="date"
+                    placeholder="Date"
+                    onChange={this.handleChange}
+                    value={this.state.newIssue.date}
+                />
+
+                <input
+                    type="text"
+                    name="issue"
+                    placeholder="Issue"
+                    onChange={this.handleChange}
+                    value={this.state.newIssue.issue}
+                />
+                <input
+                            type = "text"
+                            name = "description"
+                            placeholder = "Description"
+                            onChange = {this.handleChange}
+                            value = {this.state.newIssue.discription}
+                            />
+                            <button>Submit</button>
+            </form>
+
+
+        </div>);
+    }
+}
+
+export default IssueForm;
