00790ca8d55607c1bd56cab4e485bed51531204a
Andrew <andrewryanogle@gmail.com>
2019-02-17 22:15:21 -0800

    added styling for mobile menue button

--- irsr/src/Components/Nav.js
+++ irsr/src/Components/Nav.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import '../css/index.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = (props) => {
     return (
-        <div className = 'navBarWrapper'>
-        <i class="fas fa-bars"></i>
-        <nav className="navBar">
-            <h2>International Rual School Report</h2>
-            <div>
-                <div className = "navLinkWrapper">
-                <a class="fas fa-th"></a>
-                <Link to = {'/home'}>Issues</Link>
+        <div className='navBarWrapper'>
+            <button className='barsButton'><i class="fas fa-bars"></i></button>
+            <nav className="navBar">
+                <h2>International Rual School Report</h2>
+                <div>
+                    <div className="navLinkWrapper">
+                        <i class="fas fa-th menuIcon"></i>
+                        <Link to={'/home'}>Issues</Link>
+                    </div>
+                    <div className="navLinkWrapper">
+                        <i class="fas fa-pen-square menuIcon"></i>
+                        <Link to={'/home/issueform'}>New Issue</Link>
+                    </div>
+                    <div className="navLinkWrapper">
+                        <i className="fas fa-sign-in-alt menuIcon"></i>
+                        <a>Log Out</a>
+                    </div>
                 </div>
-                <div className = "navLinkWrapper">
-                <i class="fas fa-pen-square"></i>
-                <Link to = {'/home/issueform'}>New Issue</Link>
-                </div>
-                <div className = "navLinkWrapper">
-                <i className="fas fa-sign-in-alt"></i>
-                <a>Log Out</a>
-                </div>
-            </div>
-        </nav>
+            </nav>
         </div>
     );
 }
