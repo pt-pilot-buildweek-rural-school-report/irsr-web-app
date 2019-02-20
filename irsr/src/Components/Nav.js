a00655e2c6b63f1594bdaec2993a634ae09408a1
Andrew <andrewryanogle@gmail.com>
2019-02-19 19:46:41 -0800

    trying to fix mobile nav

--- irsr/src/Components/Nav.js
+++ irsr/src/Components/Nav.js
@@ -1,34 +1,49 @@
-import React from 'react';
-import '../css/index.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "../css/index.css";
+import { Link } from "react-router-dom";
 
-const Nav = (props) => {
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      condition: false
+    };
+  }
+  handleClick = event => {
+    this.setState({
+      condition: !this.state.condition
+    });
+  };
+  render() {
     return (
-        <div className='navBarWrapper'>
-            <button className='barsButton'><i class="fas fa-bars"></i></button>
-            <nav className="navBar">
-                <h2>International Rual School Report</h2>
-                <div>
-                    <div className="navLinkWrapper">
-                        <i class="far fa-user menuIcon"></i>
-                        <Link to={'/home/profile'}>Profile</Link>
-                    </div>
-                    <div className="navLinkWrapper">
-                        <i class="fas fa-th menuIcon"></i>
-                        <Link to={'/home'}>Issues</Link>
-                    </div>
-                    <div className="navLinkWrapper">
-                        <i class="fas fa-pen-square menuIcon"></i>
-                        <Link to={'/home/issueform'}>New Issue</Link>
-                    </div>
-                    <div className="navLinkWrapper">
-                        <i className="fas fa-sign-in-alt menuIcon"></i>
-                        <a href = '#'>Log Out</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
+      <div className="navBarWrapper">
+        <button onClick={this.handleClick} className="barsButton">
+          <i className="fas fa-bars" />
+        </button>
+        <nav className={this.state.condition ? "navBar toggled" : "navBar"}>
+          <h2>International Rual School Report</h2>
+          <div>
+            <div className="navLinkWrapper">
+              <i className="far fa-user menuIcon" />
+              <Link to={"/home/profile"}>Profile</Link>
+            </div>
+            <div className="navLinkWrapper">
+              <i className="fas fa-th menuIcon" />
+              <Link to={"/home"}>Issues</Link>
+            </div>
+            <div className="navLinkWrapper">
+              <i className="fas fa-pen-square menuIcon" />
+              <Link to={"/home/issueform"}>New Issue</Link>
+            </div>
+            <div className="navLinkWrapper">
+              <i className="fas fa-sign-in-alt menuIcon" />
+              <a href="#">Log Out</a>
+            </div>
+          </div>
+        </nav>
+      </div>
     );
+  }
 }
 
 export default Nav;
